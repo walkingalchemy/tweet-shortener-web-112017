9196a965350fdba2e1104f007f42597d4ac9aca3
James Royer <walkingalchemy@gmail.com>
2017-11-04 02:45:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,7 +38,10 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length < 140
-    word_substituter(tweet)
+  if tweet == nil
+    return
+  end
+  tweet.length < 140 ?
+    word_substituter(tweet) : tweet
   end
 end
