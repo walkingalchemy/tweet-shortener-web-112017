b01cd35bfa51589063623f9b41af7c9a75ed5b64
James Royer <walkingalchemy@gmail.com>
2017-11-04 02:36:58 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,29 +1,44 @@
 require 'pry'
 
 # Write your code here.
-dict = {
-  hello: "hi",
-  to: "2",
-  two: "2",
-  too: "2",
-  four: "4",
-  for: "4",
-  be: "b",
-  you: "u",
-  at: "@",
-  and: "&",
-
-}
+def dictionary
+  {
+    hello: "hi",
+    to: "2",
+    two: "2",
+    too: "2",
+    four: "4",
+    for: "4",
+    be: "b",
+    you: "u",
+    at: "@",
+    and: "&",
+  }
+end
 
 def word_substituter(tweet)
   new_tweet = tweet.split
+  dict = dictionary
   new_tweet.each_with_index do |word, index|
     # binding.pry
-    if word == dict.select {|key, substitute| key.to_s}
-      binding.pry
-      new_tweet[index] = dict[word]
+    if dict.key?(word.downcase.to_sym)
+    # if word == dict.select {|key, substitute| key.to_s}
+      new_tweet[index] = dict[word.downcase.to_sym]
+      # binding.pry
     end
   end
-  new_tweet.join
+  new_tweet = new_tweet.join(" ")
+  # binding.pry
 end
-word_substituter("hello be at you You u four for two too to To and :: These words should not get changed. ")
+# word_substituter("hello be at you You u four for two too to To and :: These words should not get changed. ")
+
+
+def bulk_tweet_shortener(tweets)
+  tweets.each {|tweet| puts "#{word_substituter(tweet)}"}
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length < 180
+    word_substituter(tweet)
+  end
+end
